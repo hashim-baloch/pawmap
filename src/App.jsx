efb6decccbaf5d792053d3b9e53f96b2fe5f0831
hashim-balouch <hashimcpro@gmail.com>
2025-01-19 20:29:48 +0500

    feat: update styles and add logo to enhance UI + remove unused assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Map from "./components/Map";
 import "./App.css";
-
+import logo from "./assets/logo.svg";
 function App() {
   return (
     <>
+      <a href="/">
+        <img src={logo} alt="PawMap" className="logo" />
+      </a>
       <Map />
     </>
   );
